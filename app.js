6739aad9157b13b8ab6dbfb9a0a5f4a903418ddd
WilliamChen15 <turelight13@gmail.com>
2022-12-05 22:54:55 +0800

    revice code and color of rate icon

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const restaurantList = require('./restaurant.json')
+const restaurantList = require('./restaurant.json').results
 
 // require express-handlebars here
 const exphbs = require('express-handlebars')
@@ -16,21 +16,18 @@ app.use(express.static('public'))
 
 // routes setting
 
-// V
 app.get('/', (req, res) => {
-  res.render('index', { restaurants: restaurantList.results })
+  res.render('index', { restaurants: restaurantList })
 })
 
-// V
 app.get('/restaurants/:restaurant_id', (req, res) => {
-  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  const restaurant = restaurantList.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
   res.render('show', { restaurant: restaurant })
 })
 
-// V
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
-  const restaurants = restaurantList.results.filter(restaurant => {
+  const restaurants = restaurantList.filter(restaurant => {
     return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase())
   })
   res.render('index', { restaurants: restaurants, keyword: keyword })
